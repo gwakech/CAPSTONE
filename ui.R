4d4468c0f001b3eb81f43212cba441fc0c0ea403
George A. W. Akech <georgewasonga@yahoo.com>
2017-02-12 21:41:31 -0600

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,54 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+
+library(shiny)
+library(markdown)
+
+
+## SHINY UI
+shinyUI(
+  fluidPage(
+    titlePanel("FINAL PROJECT - DATA SCIENCE CAPSTONE - USING NLP TO PREDICT WORDS"),
+    sidebarLayout(
+      sidebarPanel(
+        helpText("ENTER A WORD, TEXT OR A SENTENCE TO PREVIEW NEXT WORD PREDICTION"),
+        hr(),
+        textInput("inputText", "ENTER THE TEXT / WORD / SENTENCE HERE",value = ""),
+        hr(),
+        helpText("1 - AFTER THE TEXT INPUT THE PREDICT NEXT WORD WILL BE DISPLAYED.", 
+                 hr(),
+                 "2 - YOU HAVE TO ENTER A PARTIALLY TEXT /SENTENCE TO SHOW THE NEXT WORD PREDICTION.",
+                 hr(),
+                 "3 - THE FORWARD WORD IS SHOWED AT THE PREDICT NEXT WORD TEXT BOX ON THE RIGHT SIDE"),
+        hr(),
+        hr()
+      ),
+      mainPanel(
+        h2("FOLLOW THE PREDICT NEXT WORD AT THIS BOX"),
+        verbatimTextOutput("prediction"),
+        strong("WORD / TEXT / SENTENCE ENTERED:"),
+        strong(code(textOutput('sentence1'))),
+        br(),
+        strong("USING SEARCH AT N-GRAMS TO SHOW NEXT WORD:"),
+        strong(code(textOutput('sentence2'))),
+        hr(),
+        hr(),
+        hr(),
+        img(src = 'swiftkey_logo.jpg', height = 101, width = 498),
+        hr(),
+        hr(),
+        img(src = 'coursera_logo.png', height = 122, width = 467),
+        hr()
+      )
+    )
+  )
+)
+
+
